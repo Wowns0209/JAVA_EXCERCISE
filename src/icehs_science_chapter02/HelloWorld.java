c62608a3c99316c3c746643bf7599d7fe4e53f30
admin <admin@10.137.138.161>
2018-01-02 16:50:08 +0900

    chapter02

---
+++ src/icehs_science_chapter02/HelloWorld.java
@@ -0,0 +1,16 @@
+package icehs_science_chapter02;
+
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		System.out.println("棲橡掉 傷梃塭~六六六六");
+		System.out.println("棲橡掉 傷梃塭~六六六六");
+		System.out.println("棲橡掉 傷梃塭~六六六六");
+		System.out.println("棲橡掉 傷梃塭~六六六六");
+		System.out.println("棲橡掉 傷梃塭~六六六六");
+		System.out.println("棲橡掉 傷梃塭~六六六六");
+		System.out.println("棲橡掉 傷梃塭~六六六六");
+		System.out.println("棲橡掉 傷梃塭~六六六六");
+	}
+
+}
